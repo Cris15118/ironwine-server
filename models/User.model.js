1430351eda211bf70e5b41c535f03ea3c5cf8493
Fatima Garcia <fagarmartin@gmail.com>
2023-05-31 19:19:36 +0200

    User model done

--- models/User.model.js
+++ models/User.model.js
@@ -1,8 +1,14 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, default: mongoose } = require("mongoose");
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    username:{
+      type:String,
+      required:[true, 'Username is required.'],
+      unique:true
+    },
+
     email: {
       type: String,
       required: [true, 'Email is required.'],
@@ -13,7 +19,15 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: [true, 'Password is required.']
-    }
+    },
+    role:{
+        type:String,
+        enum: ["user","admin"]
+    },
+    wishList :[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    }]
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`    
