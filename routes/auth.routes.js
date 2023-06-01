ba5888c13c4414307e03098834e1845ed4c7fa53
Cristina Tabares <cristina_tabares@hotmail.es>
2023-06-01 11:27:01 +0200

    cambiar bcryptjs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const User = require("../models/User.model");
 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const isAuthenticated=require("../middlewares/isAuthenticated")
 
