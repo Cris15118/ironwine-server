57c2b8949674b0c4807d58e166ba0aa3400d11d9
Fatima Garcia <fagarmartin@gmail.com>
2023-06-01 15:47:40 +0200

    formularios de admin a falta de probar la imagen

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -3,12 +3,11 @@ const router = require("express").Router();
 const Product = require("../models/Product.model");
 const uploader= require("../middlewares/cloudinary.config")
 
-//POST "/admin/create" recoge los datos de creación de productos y los añade a la BD
+//POST "/api/admin/create" recoge los datos de creación de productos y los añade a la BD
 
-//router.post("/create",uploader.single("image"),async(req,res,next)=>{
-    router.post("/create",async(req,res,next)=>{
+//router.post("/api/create",uploader.single("image"),async(req,res,next)=>{
+router.post("/create",async(req,res,next)=>{
 const {name,description,price,tipo,bodega,stock} = req.body 
-
 
 try{
     await Product.create({
@@ -27,6 +26,42 @@ catch(err)
 
 })
 
+//PUT "/api/admin/:id" recoge datos de edición de producto y edita en la BD
+router.put("/:id",async(req,res,next)=>{
+
+    const {id} = req.params
+    const {name,description,price,tipo,bodega,stock} = req.body 
+
+    if(!name || !description || !price || !tipo || !bodega || !stock)
+    {
+        res.json("Los campos deben de estar llenos")
+        return
+    }
+
+    try{
+        const response = await Product.findByIdAndUpdate(id,{name,description,price,tipo,bodega,stock},{new:true})
+        res.json("documento actualizado")
+    }
+    catch(err)
+    {
+        console.log(err)
+        next(err)
+    }
+})
+
+//DELETE "/api/admin/:id" borra un producto
+router.delete("/:id",async(req,res,next)=>{
+    const {id} =req.params
+    try{
+
+        await Product.findByIdAndDelete(id)
+        res.json("Documento borrado")
+    }
+    catch(err)
+    {
+        next(err)
+    }
+})
 
 
 
