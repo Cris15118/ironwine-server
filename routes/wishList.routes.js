230b2f57dc4bcd1f9fd38995ebccfcef82fad9da
Fatima Garcia <fagarmartin@gmail.com>
2023-06-06 19:27:40 +0200

    añadida ruta comprobar si esta en lista de deseos

--- routes/wishList.routes.js
+++ routes/wishList.routes.js
@@ -19,6 +19,47 @@ router.get("/", isAuthenticated, async (req, res, next) => {
   }
 });
 
+//GET "/api/wishlist/in/:productId" devuelve si ese usuario tiene ese producto en su lista de deseos
+router.get("/in/:productId", isAuthenticated, async (req, res, next) => {
+  const userId = req.payload._id;
+  
+  const productId=req.params.productId
+  console.log(userId,"userId",productId)
+  try {
+    const response= await User.findOne({
+      $and: [{ _id: userId }, {wishList:{ "$in" :[productId] }}],
+    });
+
+   // { favouriteFoods: { "$in" : ["sushi"]} }
+   if(response)
+   {
+    res.json(true);
+   }
+   else{
+    res.json(false);
+   }
+    
+  } catch (error) {
+    next(error);
+    console.log(error);
+  }
+});
+//GET "/api/wishlist" devuelve todos los productos de la lista de deseos
+router.get("/", isAuthenticated, async (req, res, next) => {
+  const userId = req.payload._id;
+  try {
+    const response = await User.findById(userId).populate(
+      "wishList.",
+      "name image price"
+    ).select({wishlist:1})
+  
+    res.json(response.wishList);
+  } catch (error) {
+    next(error);
+    console.log(error);
+  }
+});
+
 //PATH "/api/wishlist/:productId/add" añade un producto a la lista de deseos del usuario
 router.patch("/:productId/add", isAuthenticated, async (req, res, next) => {
   const idUser = req.payload._id;
