a315310cdeb0267053d27e32ddcfa25846b33a60
Fatima Garcia <fagarmartin@gmail.com>
2023-06-08 19:54:02 +0200

    auth 401

--- error-handling/index.js
+++ error-handling/index.js
@@ -5,17 +5,18 @@ module.exports = (app) => {
   });
 
   app.use((err, req, res, next) => {
-    // whenever you call next(err), this middleware will handle the error
-    // always logs the error
-    console.error("ERROR", req.method, req.path, err);
-
+    if (err.status === 401) {
+      //de jwt
+      res.status(401).json({
+        message: "Token no valido o no entregado",
+      });
+      return;
+    }
     // only render if the error ocurred before sending the response
     if (!res.headersSent) {
-      res
-        .status(500)
-        .json({
-          message: "Internal server error. Check the server console",
-        });
+      res.status(500).json({
+        message: "Internal server error. Check the server console",
+      });
     }
   });
 };
